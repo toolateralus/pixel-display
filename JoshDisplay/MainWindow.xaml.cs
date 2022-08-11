ac33a41b233aad80fc157b66f51a58102722816f
toolateralus <109556963+toolateralus@users.noreply.github.com>
2022-08-11 15:34:26 -0400

    added more usability and functionality to animation and color

--- JoshDisplay/MainWindow.xaml.cs
+++ JoshDisplay/MainWindow.xaml.cs
@@ -16,25 +16,25 @@ using System.Windows.Threading;
 
 namespace JoshDisplay
 {
-    
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            outputGrid.ShowGridLines = false; 
+            outputGrid.ShowGridLines = false;
         }
         private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+
         }
         private void Accept_Clicked(object sender, RoutedEventArgs e)
         {
-            RefreshRateClock(TimeSpan.FromSeconds(float.Parse(UpdateIntervalInSecs.Text)));  
+            RefreshRateClock(TimeSpan.FromSeconds(float.Parse(UpdateIntervalInSecs.Text)));
         }
-        bool randomColor = false;
-        bool moving = true;
-        bool movingX = true;
+        bool scrollingColor = false;
+        bool moving = false;
+        bool movingX = false;
         bool movingY = false;
         private bool cleaning;
 
@@ -42,57 +42,64 @@ namespace JoshDisplay
         public void RefreshDisplay(object? sender, EventArgs e)
         {
             WipeDisplay();
-            if (cleaning) return; 
+            if (cleaning) return;
             // length of one side of the screen; a square.
             Int32.TryParse(displayAreaXText.Text, out int displayAreaX);
             Int32.TryParse(displayAreaYText.Text, out int displayAreaY);
             //!! starts in the center if displayArea == 3; && offset == 6;!! 
-           
+
+            Int32.TryParse(displayOffsetXText.Text, out int offsetX);
+            Int32.TryParse(displayOffsetYText.Text, out int offsetY);
+
+            if (moving)
+            {
+                if (movingX)
+                {
+                    if (offsetX < 16) offsetX++;
+                    else if (offsetY < 16)
+                    {
+                        offsetY++;
+                        offsetX = 0;
+                    }
+                    else offsetY = 0;
+                }
+                if (movingY)
+                {
+                    if (offsetY < 16) offsetY++;
+                    else if (offsetX < 16)
+                    {
+                        offsetX++;
+                        offsetY = 0;
+                    }
+                    else offsetX = 0;
+                }
+                displayOffsetXText.Text = offsetX.ToString();
+                displayOffsetYText.Text = offsetY.ToString();
+            }
             for (int i = 0; i < displayAreaX; i++)
             {
+                //PixelProcessing.Pixel Vec = PixelProcessing.FormatPixelColor(Interface.GetColorValues(inputBox.Text));
+                //// Scroll through colors 
+                //if (scrollingColor)
+                //{
+                //    if (Vec.r < 255) Vec.r++; else Vec.r = 0;
+                //    if (Vec.g < 255) Vec.g++; else Vec.g = 0;
+                //    if (Vec.b < 255) Vec.b++; else Vec.b = 0;
+                //}
                 for (int j = 0; j < displayAreaY; j++)
                 {
                     // get color from text box text string (0,0,0) alpha nyi 
                     PixelProcessing.Pixel Vec = PixelProcessing.FormatPixelColor(Interface.GetColorValues(inputBox.Text));
-                    Brush color = new SolidColorBrush(Color.FromArgb((byte)Vec.a, (byte)Vec.r, (byte)Vec.g, (byte)Vec.b));
-                    if (randomColor)
+                    if (scrollingColor)
                     {
-                        if (Vec.r < 255) Vec.r++; else Vec.r = 0; 
-                        if (Vec.g < 255) Vec.g++; else Vec.g = 0; 
-                        if (Vec.b< 255) Vec.b++; else Vec.b = 0; 
+                        if (Vec.r < 255) Vec.r++; else Vec.r = 0;
+                        if (Vec.g < 255) Vec.g++; else Vec.g = 0;
+                        if (Vec.b < 255) Vec.b++; else Vec.b = 0;
+                        inputBox.Text = $"{Vec.a},{Vec.r},{Vec.g},{Vec.b}";
                     }
-                    inputBox.Text = $"{Vec.a},{Vec.r},{Vec.g},{Vec.b}";
+                    Brush color = new SolidColorBrush(Color.FromArgb(255, (byte)Vec.r, (byte)Vec.g, (byte)Vec.b));
 
-                    
                     // offset text, used to determine X,Y origin of pixel draw, area expanding down right
-                    Int32.TryParse(displayOffsetXText.Text, out int offsetX);
-                    Int32.TryParse(displayOffsetYText.Text, out int offsetY);
-
-                    if (moving)
-                    {
-                        if (movingX)
-                        {
-                            if (offsetX < 16) offsetX++;
-                            else if (offsetY < 16)
-                            {
-                                offsetY++;
-                                offsetX = 0;
-                            }
-                            else offsetY = 0;
-                        }
-                        if (movingY)
-                        {
-                            if (offsetY < 16) offsetY++;
-                            else if (offsetX < 16)
-                            {
-                                offsetX++;
-                                offsetY = 0;
-                            }
-                            else offsetX = 0; 
-                        }
-                        displayOffsetXText.Text = offsetX.ToString(); 
-                        displayOffsetYText.Text = offsetY.ToString(); 
-                    }
 
                     outputTextBox.Content = $" Current Color: \n in (R,G,B) {Vec.r} {Vec.g} {Vec.b} \n " +
                         $"xOffset: {offsetX} yOffset: {offsetY} \n xArea: {displayAreaX} yArea: {displayAreaY}";
@@ -108,18 +115,29 @@ namespace JoshDisplay
             outputGrid.UpdateLayout();
         }
 
-        public void RefreshRateClock(TimeSpan interval)
+        public void RefreshRateClock(TimeSpan? interval)
         {
+            try
+            {
+                if (interval == null)
+                    return;
+            }
+            catch (Exception e)
+            {
+                outputTextBox.Content = e.Message;
+            }
             var timer = new DispatcherTimer();
             timer.Tick += RefreshDisplay;
-            timer.Interval = interval;
+#pragma warning disable CS8629 // Nullable value type may be null.
+            timer.Interval = (TimeSpan)interval;
+#pragma warning restore CS8629 // Nullable value type may be null.
 
             if (timer.IsEnabled)
             {
                 timer.Stop();
-                return; 
+                return;
             }
-            timer.Start(); 
+            timer.Start();
         }
         private void Reset_buttonClick(object sender, RoutedEventArgs e)
         {
@@ -128,16 +146,17 @@ namespace JoshDisplay
         // Cleans all rectangles off screen
         void WipeDisplay()
         {
-            if (cleaning) return; 
+            if (cleaning) return;
             cleaning = true;
-            List<int> list = new List<int>(); 
+            List<int> list = new List<int>();
             for (int i = 0; i < outputGrid.Children.Count; i++)
             {
                 // ugly IF to check type
                 if (outputGrid.Children[i].GetType() != typeof(Button)
                 && outputGrid.Children[i].GetType() != typeof(RichTextBox)
                 && outputGrid.Children[i].GetType() != typeof(Label)
-                && outputGrid.Children[i].GetType() != typeof(TextBox))
+                && outputGrid.Children[i].GetType() != typeof(TextBox)
+                && outputGrid.Children[i].GetType() != typeof(CheckBox))
 
                 { list.Add(i); }
             }
@@ -156,8 +175,38 @@ namespace JoshDisplay
                 }
             }
             outputGrid.UpdateLayout();
-            cleaning = false; 
+            cleaning = false;
         }
+
+
+        private void ScrollX_checked(object sender, RoutedEventArgs e)
+        {
+            movingX = true;
+        }
+        private void ScrollX_unchecked(object sender, RoutedEventArgs e)
+        {
+            movingX = false;
+        }
+
+        private void ScrollY_checked(object sender, RoutedEventArgs e)
+        {
+            movingY = true;
+        }
+        private void ScrollY_unchecked(object sender, RoutedEventArgs e)
+        {
+            movingY = false;
+        }
+
+        private void ScrollingColor_checked(object sender, RoutedEventArgs e)
+        {
+            scrollingColor=true;
+        }
+        private void ScrollingColor_unchecked(object sender, RoutedEventArgs e)
+        {
+            scrollingColor=false;
+        }
+
+
     }
     public static class PixelProcessing
     {
