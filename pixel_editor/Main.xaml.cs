fa96ae594424697bcf577022447c2abbe5aef637
toolateralus <109556963+toolateralus@users.noreply.github.com>
2022-11-01 10:40:40 -0400

    added editor project and scaling for editor window from https://stackoverflow.com/questions/3193339/tips-on-developing-resolution-independent-application/5000120#5000120 by JacobJ

---
+++ pixel_editor/Main.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+
+namespace pixel_editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main() => InitializeComponent();
+        #region ScaleValue Depdency Property
+        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(Main), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
+
+        private static object OnCoerceScaleValue(DependencyObject o, object value)
+        {
+            Main mainWindow = o as Main;
+            if (mainWindow != null)
+                return mainWindow.OnCoerceScaleValue((double)value);
+            else return value;
+        }
+
+        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            Main mainWindow = o as Main;
+            if (mainWindow != null)
+                mainWindow.OnScaleValueChanged((double)e.OldValue, (double)e.NewValue);
+        }
+
+        protected virtual double OnCoerceScaleValue(double value)
+        {
+            if (double.IsNaN(value))
+                return 1.0f;
+
+            value = Math.Max(0.1, value);
+            return value;
+        }
+
+        protected virtual void OnScaleValueChanged(double oldValue, double newValue) { }
+
+        public double ScaleValue
+        {
+            get => (double)GetValue(ScaleValueProperty);
+            set => SetValue(ScaleValueProperty, value);
+        }
+        #endregion
+        private void CalculateScale()
+        {
+            double yScale = ActualHeight / 250f;
+            double xScale = ActualWidth / 200f;
+            double value = Math.Min(xScale, yScale);
+
+            ScaleValue = (double)OnCoerceScaleValue(window, value);
+        }
+        public void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            CalculateScale();
+        }
+    }
+}
