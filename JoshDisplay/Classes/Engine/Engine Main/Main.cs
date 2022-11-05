bc31e0c993744953e269ebf9f119426c7571c9a9
toolateralus <109556963+toolateralus@users.noreply.github.com>
2022-11-05 15:34:27 -0400

    collision detection working slightly better, solving working

--- JoshDisplay/Classes/Engine/Engine Main/Main.cs
+++ JoshDisplay/Classes/Engine/Engine Main/Main.cs
@@ -373,34 +373,118 @@ namespace pixel_renderer
 
                 Rigidbody rbA = a.GetComponent<Rigidbody>();
                 Rigidbody rbB = b.GetComponent<Rigidbody>();
+
                 Rigidbody submissive = null;
                 Rigidbody dominant = null;
+
                 Vec2 sizeA = spriteA.size;
                 Vec2 sizeB = spriteB.size;
 
                 Vec2 posA = a.position;
                 Vec2 posB = b.position;
 
-                var distance = (posA + sizeA - posB + sizeB); 
-                if (rbA.velocity.Length > rbB.velocity.Length)
-                {
-                    dominant = rbB; 
-                    submissive = rbA;
-                }
-                else
+                Vec2 distance = posA + sizeA - posB + sizeB; 
+                if (rbA.velocity.Length >= rbB.velocity.Length)
                 {
                     dominant = rbA; 
                     submissive = rbB;
                 }
-                var depenetrationForce = 0f;
-                if (dominant.velocity.Length < 1)
+                else
                 {
-                    depenetrationForce = Constants.depenetrationForce;
+                    dominant = rbB; 
+                    submissive = rbA;
                 }
-                else depenetrationForce = Constants.depenetrationForce + dominant.velocity.Length; 
-                submissive.velocity = CMath.Negate(dominant.velocity); 
+                if (submissive == null || dominant == null)
+                {
+                    submissive = rbB;
+                    dominant = rbA;
+                }
+                submissive.parentNode.position += distance; 
+                submissive.velocity += CMath.Negate(dominant.velocity * submissive.velocity.Length * 250); 
             }
-            hash = new(Constants.screenWidth, Constants.screenHeight, Constants.collisionCellSize);
+            //hash = new(Constants.screenWidth, Constants.screenHeight, Constants.collisionCellSize);
+        }
+    }
+    public class SpatialHash
+    {
+        int rows;
+        int columns;
+        int cellSize;
+        List<List<Node>> Buckets = new List<List<Node>>();
+
+        public SpatialHash(int screenWidth, int screenHeight, int cellSize)
+        {
+            Buckets = new List<List<Node>>(); 
+            rows = screenHeight / cellSize;
+            columns = screenWidth / cellSize;
+            this.cellSize = cellSize;
+            for (int i = 0; i < columns * rows; i++)
+            {
+                Buckets.Add(new List<Node>());
+            }
+        }
+        internal void ClearBuckets()
+        {
+            for (int i = 0; i < columns * rows; i++)
+            {
+                Buckets[i].Clear();
+            }
+        }
+        internal void RegisterObject(Node obj)
+        {
+            List<int> cells = Hash(obj);
+            foreach (var index in cells)
+            {
+                if (index < 0 || index > rows * columns) continue;
+                Buckets[index].Add(obj);
+            }
+        }
+        internal List<Node> GetNearby(Node node)
+        {
+            List<Node> nodes = new List<Node>();
+            List<int> buckets = Hash(node);
+            foreach (var index in buckets)
+            {
+                if (index < 0 || index > rows * columns) continue;
+                nodes.AddRange(Buckets[index]);
+            }
+            return nodes;
+        }
+        private void AddBucket(Vec2 vector, float width, List<int> bucket)
+        {
+            int cellPosition = (int)(
+                       (Math.Floor(vector.x / cellSize)) +
+                       (Math.Floor(vector.y / cellSize)) *
+                       width
+            );
+
+            if (!bucket.Contains(cellPosition))
+                bucket.Add(cellPosition);
+
+        }
+        private List<int> Hash(Node obj)
+        {
+            Sprite sprite = obj.GetComponent<Sprite>(); 
+            List<int> bucketsObjIsIn = new List<int>();
+            Vec2 min = new Vec2(
+                obj.position.x,
+                obj.position.y);
+            Vec2 max = new Vec2(
+                obj.position.x + sprite.size.x,
+                obj.position.y + sprite.size.y);
+            float width = Constants.screenWidth / cellSize;
+            
+            //TopLeft
+            AddBucket(min, width, bucketsObjIsIn);
+            //TopRight
+            AddBucket(new Vec2(max.x, min.y), width, bucketsObjIsIn);
+            //BottomRight
+            AddBucket(new Vec2(max.x, min.y), width, bucketsObjIsIn);
+            //BottomLeft
+            AddBucket(new Vec2(max.x, min.y), width, bucketsObjIsIn);
+
+            return bucketsObjIsIn;
+
         }
     }
     public static class Staging
@@ -510,86 +594,4 @@ namespace pixel_renderer
         }
     }
 
-    public class SpatialHash
-    {
-        int rows;
-        int columns;
-        int cellSize;
-        List<List<Node>> Buckets = new List<List<Node>>();
-
-        public SpatialHash(int screenWidth, int screenHeight, int cellSize)
-        {
-            Buckets = new List<List<Node>>(); 
-            rows = screenHeight / cellSize;
-            columns = screenWidth / cellSize;
-            this.cellSize = cellSize;
-            for (int i = 0; i < columns * rows; i++)
-            {
-                Buckets.Add(new List<Node>());
-            }
-        }
-        internal void ClearBuckets()
-        {
-            for (int i = 0; i < columns * rows; i++)
-            {
-                Buckets[i].Clear();
-            }
-        }
-        internal void RegisterObject(Node obj)
-        {
-            List<int> cells = Hash(obj);
-            foreach (var index in cells)
-            {
-                if (index < 0) continue;
-                Buckets[index].Add(obj);
-            }
-        }
-        internal List<Node> GetNearby(Node node)
-        {
-            List<Node> nodes = new List<Node>();
-            List<int> buckets = Hash(node);
-            foreach (var index in buckets)
-            {
-                if (index < 0) continue;
-                nodes.AddRange(Buckets[index]);
-            }
-            return nodes;
-        }
-        private void AddBucket(Vec2 vector, float width, List<int> bucket)
-        {
-            int cellPosition = (int)(
-                       (Math.Floor(vector.x / cellSize)) +
-                       (Math.Floor(vector.y / cellSize)) *
-                       width
-            );
-
-            if (!bucket.Contains(cellPosition))
-                bucket.Add(cellPosition);
-
-        }
-        private List<int> Hash(Node obj)
-        {
-            Sprite sprite = obj.GetComponent<Sprite>(); 
-            List<int> bucketsObjIsIn = new List<int>();
-            Vec2 min = new Vec2(
-                obj.position.x - sprite.size.x,
-                obj.position.y - sprite.size.y);
-            Vec2 max = new Vec2(
-                obj.position.x + sprite.size.x,
-                obj.position.y + sprite.size.y);
-            float width = Constants.screenWidth / cellSize;
-            
-            //TopLeft
-            AddBucket(min, width, bucketsObjIsIn);
-            //TopRight
-            AddBucket(new Vec2(max.x, min.y), width, bucketsObjIsIn);
-            //BottomRight
-            AddBucket(new Vec2(max.x, min.y), width, bucketsObjIsIn);
-            //BottomLeft
-            AddBucket(new Vec2(max.x, min.y), width, bucketsObjIsIn);
-
-            return bucketsObjIsIn;
-
-        }
-    }
 }
