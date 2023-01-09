6b1409b4ba33fb4c8bc6d119458afc9e53c9cd0e
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-01-09 17:00:09 -0500

    a

--- pixel_engine/Engine/Node/Node.cs
+++ pixel_engine/Engine/Node/Node.cs
@@ -125,7 +125,7 @@ namespace pixel_renderer
                 if (type.BaseType != typeof(Component)) 
                     throw new InvalidOperationException("Cannot add generic type Component to node."); 
 
-                if (!Components.ContainsKey(type))g
+                if (!Components.ContainsKey(type))
                     Components.Add(type, new());
 
                 Components[type].Add(component);
