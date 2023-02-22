0d99ae3e2403804e65c3ae9c80166fc785459002
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-02-22 10:39:46 -0500

    A

--- pixel_editor/Source/Constants.cs
+++ pixel_editor/Source/Constants.cs
@@ -6,8 +6,6 @@ namespace pixel_editor
     {
         public static Vec2Int InspectorPosition = new(16, 1);
         public const float MouseZoomSensitivityFactor = 1.001f;
-        
-        
         public const int EditorEventQueueMaxLength = 12;
         public static int InspectorWidth = 6;
         public static int InspectorHeight = 10;
