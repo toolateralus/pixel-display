14fff44cff0de983d2dd9af0d21dd9fba5d80ab1
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-02-23 13:44:17 -0500

    aa

--- pixel_engine/Engine/Static Tools/JRandom.cs
+++ pixel_engine/Engine/Static Tools/JRandom.cs
@@ -9,16 +9,20 @@
 
         public static Direction Direction() => (Direction)Int(0, sizeof(Direction) - 1);
 
-
-        public static Color TransparentColor()
+        public static Color Color(byte aMin = 0, byte rMin = 0, byte gMin = 0, byte bMin = 0, byte aMax = 255, byte rMax = 255, byte gMax = 255, byte bMax = 255)
         {
-            byte r = Byte(),
-                 g = Byte(),
-                 b = Byte(),
-                 a = Byte();
-
-            return System.Drawing.Color.FromArgb(r, g, b, a);
+            var A = Int(aMin, aMax);
+            var R = Int(rMin, rMax);
+            var G = Int(gMin, gMax);
+            var B = Int(bMin, bMax);
+            return System.Drawing.Color.FromArgb(A, R, G, B);
         }
+        public static Color OpaqueColor()
+        {
+            return Color(255);
+        }
+
+      
         public static Color Color()
         {
             byte r = Byte(),
