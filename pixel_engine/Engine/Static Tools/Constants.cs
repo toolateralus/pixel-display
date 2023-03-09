c6dc565728ab484ef925a23c0fcb89f3cd042968
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-03-09 15:19:00 -0500

    a

--- pixel_engine/Engine/Static Tools/Constants.cs
+++ pixel_engine/Engine/Static Tools/Constants.cs
@@ -8,48 +8,6 @@ namespace pixel_renderer
 {
     public class Constants
     {
-        #region Physics / Collision Constants
-        /// <summary>
-        /// FixedUpdate / Physics refresh interval,
-        /// NOTE: for some reason this seems to have absolutely no effect other than it cannot be zero. 
-        /// this is in the way of implementing a time scale.
-        /// </summary>  
-        public const float PhysicsRefreshInterval = .01f;
-        public const float MaxDepenetrationForce = 3f;
-        public static int TerminalVelocity = 4;
-
-        public static Vector2 TerminalVec2()
-        {
-            return new Vector2()
-            {
-                X = TerminalVelocity,
-                Y = TerminalVelocity,
-            };
-        }
-        public static Vector3 TerminalVec3()
-        {
-            return new Vector3()
-            {
-                X = TerminalVelocity,
-                Y = TerminalVelocity,
-                Z = TerminalVelocity,
-            };
-        }
-        public static bool WithinTerminalVelocity(Vector2 velocity)
-        {
-            return velocity.X <= TerminalVelocity && velocity.X >= -TerminalVelocity &&
-                velocity.Y <= TerminalVelocity && velocity.Y >= -TerminalVelocity;
-        }
-        public static bool WithinTerminalVelocity(Vector3 velocity)
-        {
-            return velocity.X <= TerminalVelocity && velocity.X >= -TerminalVelocity &&
-                velocity.Y <= TerminalVelocity && velocity.Y >= -TerminalVelocity &&
-                 velocity.Z <= TerminalVelocity && velocity.Z >= -TerminalVelocity;
-        }
-        public static bool WithinTerminalVelocity(Rigidbody rigidbody) => WithinTerminalVelocity(rigidbody.velocity);
-        #endregion
-        #region General Constants
-
         public const int FramerateSampleThreshold = 60;
 
         public static Vector2 PhysicsArea = new Vector2(10000, 10000);
@@ -59,7 +17,7 @@ namespace pixel_renderer
         /// <summary>
         /// amount of time in milliseconds between physics frames (60 fps ~ 16ms)
         /// </summary>
-        public const int PhysicsTimeStep = 160; 
+        public const int PhysicsTimeStep = 16; 
 
         public static Vector2 CurrentResolution => Runtime.Current.renderHost.GetRenderer().Resolution;
         public static Vector2 DefaultResolution => new(ScreenW, ScreenH);
@@ -68,8 +26,7 @@ namespace pixel_renderer
         public static Vector2 MinResolution = new(4, 4);
 
         public static Pixel EditorHighlightColor = Color.Orange;
-        #endregion
-        #region IO
+
         public static char[] unsupported_chars = { '_', '-', '.', '`' };
         public static char[] int_chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         
@@ -95,7 +52,7 @@ namespace pixel_renderer
         };
 
         public static string WorkingRoot = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Pixel";    // Root directory for resources
-        #endregion
+
         public static List<Type> GetInheritedTypesFromBase<T>()
         {
             var types = AppDomain.CurrentDomain.GetAssemblies()
