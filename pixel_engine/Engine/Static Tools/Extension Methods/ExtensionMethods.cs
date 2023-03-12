2babc3bdbb3e4e89edb2deb482afda9233429350
Cooper-Pilot <47166588+Cooper-Pilot@users.noreply.github.com>
2023-03-12 11:47:48 -0400

    Sorted eextension methods

--- pixel_engine/Engine/Static Tools/Extension Methods/ExtensionMethods.cs
+++ pixel_engine/Engine/Static Tools/Extension Methods/ExtensionMethods.cs
@@ -12,215 +12,6 @@ namespace pixel_renderer
 {
     public static class ExtensionMethods
     {
-        public readonly static Vector2 one = new(1, 1);
-        public readonly static Vector2 zero = new(0, 0);
-        
-        private const float Epsilon = float.Epsilon;
-
-        public static Vector2 up = new(0, -1);
-        public static Vector2 down = new(0, 1);
-        public static Vector2 left = new(-1, 0);
-        public static Vector2 right = new(1, 0);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float DistanceSquared(Vector2 a, Vector2 b)
-        {
-            return (b.X - a.X) * (b.X - a.X) + (b.Y - a.Y) * (b.Y - a.Y);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float SqrDistanceFrom(this Vector2 a, Vector2 v)
-        {
-            return DistanceSquared(a, v);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float DistanceFrom(this Vector2 v,  Vector2 a)
-        {
-            return v.Distance(a);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Distance(this Vector2 a, Vector2 b)
-        {
-            var distanceSquared = DistanceSquared(a, b);
-            return CMath.Sqrt(distanceSquared);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Dot(this Vector2 a, Vector2 b)
-        {
-            return (a.X * b.X) + (a.Y * b.Y);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Length(this Vector2 v) => MathF.Sqrt(v.X * v.X + v.Y * v.Y);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 Rotated(this Vector2 v, float angle)
-        {
-            float cos = MathF.Cos(angle);
-            float sin = MathF.Sin(angle);
-            return new Vector2(cos * v.X - sin * v.Y, sin * v.X + cos * v.Y);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Rotate(this ref Vector2 v, float angle)
-        {
-            float cos = MathF.Cos(angle);
-            float sin = MathF.Sin(angle);
-            float newX = cos * v.X - sin * v.Y;
-            float newY = sin * v.X + cos * v.Y;
-            v.X = newX;
-            v.Y = newY;
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float SqrMagnitude(this Vector2 v)
-        {
-            var product = MathF.FusedMultiplyAdd(v.X, v.X, v.Y * v.Y);
-            return product; 
-        }
-        #region Numbers
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool WithinRange(this float v, float min, float max) { return v <= max && v >= min; }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool WithinRange(this int v, int min, int max) { return v <= max && v >= min; }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Clamp(this double v, double min, double max) => Math.Min(max, Math.Max(v, min));
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Squared(this double v) => v * v;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Squared(this float v) => v * v;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Clamp(this ref Vector2 v, Vector2 min, Vector2 max)
-        {
-            v.X = v.X.Clamped(min.X, max.X);
-            v.Y = v.Y.Clamped(min.Y, max.Y);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Wrap(this Vector2 v, Vector2 max) { v.X = v.X.Wrapped(max.X); v.Y = v.Y.Wrapped(max.Y); }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 Wrapped(this Vector2 v, Vector2 max) => new(v.X.Wrapped(max.X), v.Y.Wrapped(max.Y));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsWithin(this Vector2 v, Vector2 min, Vector2 max) => v.X.IsWithin(min.X, max.X) && v.Y.IsWithin(min.Y, max.Y);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsWithinMaxExclusive(this Vector2 v, Vector2 min, Vector2 max) => v.X.IsWithinMaxExclusive(min.X ,max.X) && v.Y.IsWithinMaxExclusive(min.Y, max.Y);
-     
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Clamped(this float v, float min, float max) => MathF.Min(max, MathF.Max(v, min));
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Clamp(this ref float v, float min, float max) => v = MathF.Min(max, MathF.Max(v, min));
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Wrapped(this float v, float max)
-        {
-            float result = v - max * MathF.Floor(v / max);
-
-            if (result >= max)
-                return result - max;
-
-            if (result < 0)
-                return result + max;
-
-            return result;
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsWithin(this float v, float min, float max) => v >= min && v <= max;
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsWithinMaxExclusive(this float v, float min, float max) => v >= min && v < max;
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float GetDivideSafe(this float v) => v == 0 ? Epsilon : v;
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void MakeDivideSafe(this float[] v) { for(int i = 0; i < v.Length; i++) v[i] = v[i].GetDivideSafe(); }
-        #endregion
-        #region Vectors
-        public static Vector2 ToVector(this string input)
-        {
-            var split = input.Split(',');
-            
-            if (!split.Any())
-                return default; 
-
-            string x = split.First();
-            string y = split.Last();
-
-            foreach (var _char in CommandParser.disallowed_chars)
-            {
-                if (x.Contains(_char))
-                    x = x.Replace($"{_char}", "");
-                
-                if (y.Contains(_char))
-                    y = y.Replace($"{_char}", "");
-            }
-
-            float xF = float.Parse(x);
-            float yF = float.Parse(y);
-
-            return new Vector2(xF, yF);
-
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 Transformed(this Vector2 v, Matrix3x2 matrix) => Vector2.Transform(v, matrix);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 ToVector2(this System.Windows.Point v) => new((float)v.X, (float)v.Y);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Transform(this ref Vector2 v, Matrix3x2 matrix) =>  v = Vector2.Transform(v, matrix);
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void MakeDivideSafe(this ref Vector2 v)
-        { 
-            v.X = v.X.GetDivideSafe();
-            v.Y = v.Y.GetDivideSafe();
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 GetDivideSafe(this Vector2 v)
-        {
-            v.X = v.X.GetDivideSafe();
-            v.Y = v.Y.GetDivideSafe();
-            return v;
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 WithValue(this Vector2 v, int? x = null, int? y = null) { return new Vector2(x ?? v.X, y ?? v.Y); }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 WithValue(this Vector2 v, float? x = null, float? y = null) { return new Vector2(x ?? v.X, y ?? v.Y); }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 WithScale(this Vector2 v, float x = 1, float y = 1) { return new Vector2(v.X * x, v.Y * y); }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Sum(this Vector2 v) => v.X + v.Y;
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static double Sum(this Vector3 v) => v.X + v.Y + v.Z;
-        
-        /// <summary>
-        ///  TODO: fix possible  'divide by zero'
-        ///   Normalize a vector
-        /// </summary>
-        /// <param name="v"></param>
-        /// <returns>A normalized Vector from the length of the current</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 Normalized(this Vector2 v)
-        {
-            if (v.Equals(Vector2.Zero))
-                return Vector2.Zero;
-            return v / v.Length();
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Normalize(this ref Vector2 v)
-        {
-            if (v.Equals(Vector2.Zero))
-                v = Vector2.Zero;
-            v =  v / v.Length();
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Increment2D(this ref Vector2 v, float xMax, float xMin = 0)
-        {
-            v.X++;
-            if (v.X >= xMax)
-            {
-                v.Y++;
-                v.X = xMin;
-            }
-        }
-        #endregion
         #region Matrices
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix3x2 Inverted(this Matrix3x2 matrix)
@@ -237,7 +28,33 @@ namespace pixel_renderer
             matrix.M22 = scale.Y;
         }
         #endregion
+
         #region Strings
+        public static Vector2 ToVector(this string input)
+        {
+            var split = input.Split(',');
+
+            if (!split.Any())
+                return default;
+
+            string x = split.First();
+            string y = split.Last();
+
+            foreach (var _char in CommandParser.disallowed_chars)
+            {
+                if (x.Contains(_char))
+                    x = x.Replace($"{_char}", "");
+
+                if (y.Contains(_char))
+                    y = y.Replace($"{_char}", "");
+            }
+
+            float xF = float.Parse(x);
+            float yF = float.Parse(y);
+
+            return new Vector2(xF, yF);
+
+        }
         /// <summary>
         /// Takes a string of any format and returns a integer value. If the string contains no number chars, it will return -1.
         /// </summary>
@@ -273,17 +90,8 @@ namespace pixel_renderer
             }
             return output;
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 Normal_LHS(this Vector2 v)
-        {
-            float x = v.X;
-            v.X = v.Y;
-            v.Y = -x;
-            v.Normalize();
-            return v; 
-        }
         #endregion
+
         #region Arrays
         /// <summary>
         /// 
@@ -299,6 +107,7 @@ namespace pixel_renderer
             return result.ToArray();
         }
         #endregion
+
         #region Image
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static System.Windows.Point GetNormalizedPoint(this System.Windows.Controls.Image img, System.Windows.Point pos)
