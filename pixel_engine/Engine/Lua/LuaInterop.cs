ef7a44cec253a07f2d03bface91c8949ab6bde8a
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-03-12 14:04:03 -0400

    lua print function added

--- pixel_engine/Engine/Lua/LuaInterop.cs
+++ pixel_engine/Engine/Lua/LuaInterop.cs
@@ -1,20 +1,21 @@
 ﻿using KeraLua;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-
-
 
 namespace pixel_renderer
 {
     public class LuaInterop
     {
-        Lua state;
+        private Lua state;
+        internal static LuaFunction func;
 
+        public LuaInterop()
+        {
+            state = new();
+
+            func = new(Print);
+
+            state.Register("print", func);
+        }
         public bool Run(string fileName)
         {
             state ??= new();
@@ -35,7 +36,52 @@ namespace pixel_renderer
                 return false;
             }
             return true;
+        }
+        public static int Print(IntPtr p)
+        {
+            var state = Lua.FromIntPtr(p);
+            var n = state.GetTop();
+            for (int i = 1; i <= n; ++i)
+            {
+                LuaType type = state.Type(i);
+                switch (type)
+                {
+                    case LuaType.None:
 
+                        break;
+                    case LuaType.Nil:
+                        Runtime.Log("Lua: Nil");
+                        break;
+                    case LuaType.Boolean:
+                        Runtime.Log($"Lua: {state.ToBoolean(i)}");
+                        break;
+                    case LuaType.LightUserData:
+                        Runtime.Log($"Lua: Light User Data");
+                        break;
+                    case LuaType.Number:
+                        Runtime.Log($"Lua: {state.ToNumber(i)}");
+                        break;
+                    case LuaType.String:
+                        Runtime.Log($"Lua: {state.ToString(i)}");
+                        break;
+                    case LuaType.Table:
+                        Runtime.Log($"Lua: Table");
+                        break;
+                    case LuaType.Function:
+                        Runtime.Log($"Lua: Function");
+                        break;
+                    case LuaType.UserData:
+                        Runtime.Log($"Lua: User Data");
+                        break;
+                    case LuaType.Thread:
+                        Runtime.Log($"Lua: Thread");
+                        break;
+                    default:
+                        break;
+             
+                }
+            }
+            return 0; 
         }
     }
 }
