81484b637b1cc69bc3290f47164f030e29f39cf5
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-03-13 18:50:08 -0400

    a

---
+++ pixel_engine/My Scripts/ParticleSystem.cs
@@ -0,0 +1,57 @@
+﻿using pixel_renderer.ShapeDrawing;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+
+
+namespace pixel_renderer
+{
+    public record Particle
+    {
+        public Particle(Vector2 velocity, Pixel color)
+        {
+            this.start += velocity;
+            this.end += velocity;
+            this.color = color;
+        }
+        public Vector2 start;
+        public Vector2 end;
+        public Pixel color;
+    }
+    public class ParticleSystem : Component
+    {
+        [Field] public List<Pixel> Pallette = new() { Color.Purple, Color.MediumSeaGreen, Color.MediumPurple, Color.MediumBlue };
+        [Field] public Vector2 launcherPos;
+        [Field] public int maxParticleCount = 1;
+        [Field] public int initParticleCount = 10;
+        [Field] public int maxDist = 250; 
+        [Field] public bool preWarm = true;
+        [Field] private List<Particle> state = new();
+
+        int index = 0;
+        public override void Awake()
+        {
+            state = new();
+            if(preWarm)
+                for (int i = 0; i < initParticleCount; ++i)
+                {
+                    Particle p = new(Position);
+                    state.Add(p);
+                }
+
+        }
+        public override void Update()
+        {
+            if (index >= state.Count)
+                index = 0;
+            Particle particle = state[index++];
+        }
+        public override void OnDrawShapes()
+        {
+           
+        }
+
+
+      
+    }
+}
