b36cb28c0e0c828a540204049eaf3fabc3c6e1f2
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-03-13 17:50:30 -0400

    more tof irewowrk

--- pixel_engine/My Scripts/Firework.cs
+++ pixel_engine/My Scripts/Firework.cs
@@ -1,6 +1,7 @@
 ﻿using pixel_renderer.ShapeDrawing;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
@@ -15,34 +16,72 @@ namespace pixel_renderer
 
     public class Particle
     {
-        public float maxDistFromStart = 100f;
-        public float minDistFromStart = 0f;
-        
-        public float minTrailDist = 0.1f;
-        public float maxTrailDist = 10f;
-        
         public bool IsAlive = true;
-        public bool Trails = false;
-
-        Line state = new(Vector2.Zero, Vector2.One);
-
-        public Pixel[] Pallette = new Pixel[] { Color.Purple, Color.MediumSeaGreen, Color.MediumPurple, Color.MediumBlue };
         public List<ParticleFrame> frames = new();
 
-        public void SetLifeCycle(int duration)
+        int index = 0;
+        private Vector2 origin;
+        private Vector2 extents;
+        public List<Pixel> Pallette = new() { Color.Purple, Color.MediumSeaGreen, Color.MediumPurple, Color.MediumBlue };
+
+        public void SetLifeCycle(Vector2 origin, Vector2 extents, Curve velocity, List<Pixel> colors)
         {
-            Vector2 end = state.startPoint * (float)Random.Shared.NextDouble();
-            for (int i = 0; i < duration; i++)
-                frames.Add(new(Vector2.Zero, state.startPoint, end, Pixel.White));
+            if (velocity.points.Count < colors.Count)
+                return;
+
+            this.origin = origin;
+            this.extents = extents;
+            var zero = Vector2.Zero;
+            foreach (var color in colors)
+            {
+                var pos = velocity.Next();
+                var end = pos + velocity.Next();
+                frames.Add(new(zero, pos, end, color));
+            }
         }
 
-        int index = 0;
-        public Line Next()
+        Vector2 accStart;
+        Vector2 accEnd;
+
+        public Line? Next()
         {
-            var frame = frames[index++];
-            Vector2 newPosOffset = new Vector2(0.1f, 0.1f);
-            return state = frame.Next(JRandom.Pixel(), JRandom.Vec2(Vector2.Zero, Vector2.One), 1, frame.start + newPosOffset);
+            if (frames.Count == 0)
+                return null; 
+
+            if (frames.Count <= index)
+                index = 0;
+
+            var frame = frames[index];
+            index++;
+
+            Pixel colorToLerpTo = JRandom.Pixel();
+            Vector2 newPosOffset = new Vector2(0.003f, -0.001f);
+            Vector2 newEndOffset = new Vector2(0.001f, -0.001f);
+            Vector2 additionalVel = new Vector2(0.001f, -0.001f);
+
+
+            if (accStart.IsWithin(origin, extents))
+                accStart += newPosOffset;
+            else accStart = origin;
+
+            if (accEnd.IsWithin(origin, extents))
+                accEnd += newEndOffset;
+            else accEnd = origin;
+            
+
+            return frame.Next(colorToLerpTo, additionalVel,  accEnd, accStart);
         }
+
+        int colorIndex= 0;
+
+        internal Pixel NextColor()
+        {
+            if (Pallette.Count == 0) return Pixel.Black;
+            if (colorIndex >= Pallette.Count)
+                colorIndex = 0;
+            return Pallette[colorIndex++];
+        }
+
     }
     public class ParticleFrame
     {
@@ -56,7 +95,7 @@ namespace pixel_renderer
             this.color = color;
         }
 
-        public Line Next(Pixel colorInput, Vector2 additionalVelocity, float lengthModifier, Vector2 newStart)
+        public Line Next(Pixel colorInput, Vector2 additionalVelocity, Vector2 newEnd, Vector2 newStart)
         {
             adjustColor();
             move();
@@ -74,11 +113,16 @@ namespace pixel_renderer
             }
             void move()
             {
+                start = newStart;
+                end = newEnd;
                 velocity += additionalVelocity;
-                end.X *= lengthModifier;
-                end.Y *= lengthModifier;
-                start += velocity;
-                end += velocity;
+
+                if (velocity != Vector2.Zero)
+                {
+                    start *= velocity;
+                    end *= velocity;
+                }
+               
             }
         }
 
@@ -86,17 +130,13 @@ namespace pixel_renderer
         public Vector2 end;
         public Pixel color;
     }
-
     public class Firework : Component
     {
         [Field] public Vector2 launcherPos;
         
         [Field] public int maxParticleCount = 250;
-        [Field] public int initParticleCount = 100;
-        
-        [Field] public int maxLifeCycleLength = 1000; 
-        [Field] public int minLifeCycleLength = 100; 
-
+        [Field] public int initParticleCount = 10;
+        [Field] public int maxDist = 1000; 
         [Field] public bool preWarm = true;
 
         Queue<Line> lines = new();
@@ -107,43 +147,40 @@ namespace pixel_renderer
 
         public override void Awake()
         {
+            drawing = true;
             state = new();
             if(preWarm)
                 for (int i = 0; i < initParticleCount; ++i)
                 {
-                    Particle p = new() 
-                    {
-                        // distance from launcher
-                        maxDistFromStart = 300,
-                        minDistFromStart = 0,
-
-                        // distance from particle
-                        maxTrailDist = 300,
-                        minTrailDist = 0,
-
-                        // other flags
-                        IsAlive = false,
-                        Trails = false,
-                    };
-                    int duration = (int)(random.NextDouble() * maxLifeCycleLength - minLifeCycleLength);
-                    p.SetLifeCycle(duration);
+                    Particle p = new();
+                    int duration = (int)(random.NextDouble() * maxDist);
+                    p.SetLifeCycle(launcherPos, launcherPos * maxDist, Curve.Circlular(1, 16, 16, true), p.Pallette);
                     state.Add(p);
                 }
+
+            drawing = false; 
 
         }
         public override void OnDrawShapes()
         {
             drawing = true;
-                for(int i = 0; lines.Count > 0; ++i)
-                    ShapeDrawer.DrawLine(lines.Dequeue());
-
+            for (int i = 0; lines.Count > 0; ++i)
+            {
+                var line = lines.Dequeue();
+                ShapeDrawer.DrawLine(line.startPoint, line.endPoint, state[i].NextColor());
+            }
             drawing = false;
         }
         public override void Update()
         {
             if (!drawing)
                 foreach (var particle in state)
-                   lines.Enqueue(particle.Next());
+                {
+                    var next = particle.Next();
+                    if (next is not null && lines.Count < maxParticleCount)
+                            lines.Enqueue(next);
+                } 
+
         }
     }
 }
