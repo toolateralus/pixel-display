e320c3f181049d5b5fa947d9a293e9891cf7d342
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-03-13 20:20:54 -0400

    more to particles

--- pixel_engine/My Scripts/ParticleSystem.cs
+++ pixel_engine/My Scripts/ParticleSystem.cs
@@ -14,16 +14,12 @@ namespace pixel_renderer
         public Vector2 velocity;
         public Vector2 position;
         public Action<Particle> lifetime;
-
-        public float lifespan;
         internal bool dead;
-
         public Particle(Vector2 initVel, Action<Particle> lifetime)
         {
             velocity = initVel;
             this.lifetime = lifetime;
         }
-
         public void Next() => lifetime.Invoke(this);
     }
     public class ParticleSystem : Component
@@ -32,7 +28,60 @@ namespace pixel_renderer
         [Field] private float maxParticleSpeed = 15f;
         [Field] private List<Particle> particles = new();
         [Field] private Random random = new();
+        public override void Awake()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                float speed;
+                Vector2 initVel = GetRandomVelocity(3);
 
+                Particle particle = new(initVel, Cycle);
+                particles.Add(particle);
+
+                
+            }
+        }
+        public override void OnDrawShapes()
+        {
+            lock (particles)
+               foreach (var p in particles)
+               {
+                    if (p.dead)
+                        continue;
+
+                    p.Next();
+                    ShapeDrawer.DrawCircle(p.position, p.velocity.Length(), p.color);
+               }
+
+        }
+        public virtual void Cycle(Particle p)
+        {
+            if (p.velocity.SqrMagnitude() < 0.5f)
+            {
+
+                OnParticleDied(p);
+                return;
+            }
+
+            p.position += p.velocity;
+            p.velocity *= 0.99f;
+
+            var col = Pixel.White;
+            _ = color();
+
+            async Task color()
+            {
+                float j = 0;
+                while (j <= 1)
+                {
+                    p.color = Pixel.Lerp(p.color, col, j);
+                    j += 0.01f;
+                    await Task.Delay(1);
+                }
+                return;
+            }
+
+        }
         public void OnParticleDied(Particle p)
         {
             if (p.dead)
@@ -44,47 +93,6 @@ namespace pixel_renderer
 
             p.dead = false; 
         }
-        public override void Awake()
-        {
-            for (int i = 0; i < 10; ++i)
-            {
-                float speed;
-                Vector2 initVel = GetRandomVelocity();
-
-                Particle particle = new(initVel, particleLifetime);
-                particles.Add(particle);
-
-                void particleLifetime(Particle p)
-                {
-                    if (p.velocity.SqrMagnitude() < 0.5f)
-                    {
-
-                        OnParticleDied(p);
-                        return;
-                    }
-
-                    p.position += p.velocity;
-                    p.velocity *= 0.99f;
-
-                    var col = Pixel.White;
-                    _ = color();
-
-                    async Task color()
-                    {
-                        float j = 0;
-                        while (j <= 1)
-                        {
-                            p.color = Pixel.Lerp(p.color, col, j);
-                            j += 0.01f;
-                            await Task.Delay(1);
-                        }
-                        return;
-                    }
-
-                }
-            }
-        }
-
         private Vector2 GetRandomVelocity(float speed = -1f)
         {
             float x = (float)random.NextDouble() * 2 - 1;
@@ -94,26 +102,5 @@ namespace pixel_renderer
              speed = (float)random.NextDouble() * maxParticleSpeed;
             return (dir * speed);
         }
-
-        public override void Update()
-        {
-           
-        }
-        public override void OnDrawShapes()
-        {
-            lock (particles)
-               foreach (var p in particles)
-               {
-                    if (p.dead)
-                        continue;
-
-                    p.Next();
-                    ShapeDrawer.DrawCircle(p.position, p.velocity.SqrMagnitude() + 2, p.color);
-               }
-
-        }
-
-
-      
     }
 }
