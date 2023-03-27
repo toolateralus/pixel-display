46f9c1b57c5b758377b3f9d27a564771f5b18965
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-03-27 18:55:54 -0400

    Native.h random image data class, unused.

--- PixelNative/PixelNative.h
+++ PixelNative/PixelNative.h
@@ -1,23 +1,51 @@
 #pragma once
 
 using namespace System;
+#include <cstring>
 
 namespace PixelNative {
-	public ref class Native
-	{
-		char* frame;
-		
-		public:
-		void SetPixel(int position, int color)
-		{
-			frame[position] = static_cast<char>(color); 
-		};
+    public ref class Native
+    {
+        char* frame;
 
-		char GetPixel(int position)
-		{
-			return frame[position];
-		}
+    public:
+        Native(int length)
+        {
+            frame = new char[length];
+        }
 
+        ~Native()
+        {
+            delete[] frame;
+        }
 
-	};
+        void SetData(char* data)
+        {
+            frame = data;
+        }
+
+        char* GetData()
+        {
+            return frame;
+        }
+
+        void SetPixel(int position, int color)
+        {
+            frame[position] = static_cast<char>(color);
+        }
+
+        char GetPixel(int position)
+        {
+            return frame[position];
+        }
+
+        property int Length
+        {
+            int get()
+            {
+                return static_cast<int>(std::strlen(frame));
+            }
+        }
+    };
 }
+
