451720142de7b2b7875e51f2a4d5425771e55a3c
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-04-28 13:30:13 -0400

    made render target in wpf window alrger

---
+++ pixel_core/Types/Attributes/HideFromEditorAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pixel_Core.Types.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class HideFromEditorAttribute : Attribute
+    {
+        /// <summary>
+        /// This allows you to prevent a type from being added to AddComponent menu atm.
+        /// </summary>
+        public HideFromEditorAttribute()
+        {
+        }
+    }
+}
