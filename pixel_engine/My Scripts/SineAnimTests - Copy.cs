43429ca689ee3b57fa925f8fd7676018a7f579dd
toolateralus <109556963+toolateralus@users.noreply.github.com>
2023-04-29 17:23:31 -0400

    asdas

---
+++ pixel_engine/My Scripts/SineAnimTests - Copy.cs
@@ -0,0 +1,48 @@
+﻿using Pixel;
+using Pixel.Types.Components;
+using System.Numerics;
+using System.Windows.Input;
+using Pixel.Types;
+using static Pixel.Input;
+using static Pixel.ShapeDrawer;
+using static Pixel.Runtime;
+using System;
+
+namespace Pixel
+{
+    public class Fire : Component
+    {
+        bool pressed = false;
+        bool pressedThisFrame = false;
+        Key key = Key.Y;
+        public override void Dispose()
+        {
+
+        }
+        public override void Awake()
+        {
+            
+            Camera.First.node?.Destroy();
+            Node camera = new("thisCamera");
+            camera.AddComponent<Camera>();
+
+
+            RegisterAction(this, OnKeyDown_Y, key);
+            RegisterAction(this, OnKeyUp_Y, key, InputEventType.KeyUp);
+        }
+        public override void FixedUpdate(float delta)
+        {
+
+            pressedThisFrame = false;
+        }
+        private void OnKeyUp_Y()
+        {
+            pressed = false;
+        }
+        private void OnKeyDown_Y()
+        {
+            pressed = true;
+            pressedThisFrame = true;
+        }
+    }
+}
